88c65c0fa0a74a2bb944a65cde487f965866f3a7
virginiablesa <43244317+virginiablesa@users.noreply.github.com>
2018-09-16 19:17:44 +0200

    Update Event.js

--- 4/src/Event.js
+++ 4/src/Event.js
@@ -1,3 +1,22 @@
+/** Class representing an event. */
 export default class Event{
-// implement your code here...
-};
+    /**
+     * Create an event with params.
+     * @param {Object} event - Data event
+     * @param {Function} action - action to run.
+     */
+    constructor(event, action) {
+        this.event = event;
+        this.method = event.type;
+        this.action = action;
+    }
+
+    /**
+     * return action defined on the event.
+     * @param {...object} params - Params that action needs
+     * @return {Function}
+     */
+    action(...params) {
+      return this.action(...params)
+    }
+  };
