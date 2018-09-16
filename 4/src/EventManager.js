6b225d4b8ec3b04466e5e02a70c489c87179ab38
virginiablesa <43244317+virginiablesa@users.noreply.github.com>
2018-09-16 19:18:02 +0200

    Update EventManager.js

--- 4/src/EventManager.js
+++ 4/src/EventManager.js
@@ -1,6 +1,40 @@
+/** Class representing an event manager. */
 export default class EventManager{
+    /**
+     * Create an event manager with params.
+     * @param {Object} events - Events created
+     * @param {Object} eventsData - Data Events.
+     */
+    constructor(events, eventsData) {
+      this.events = events;
+      this.eventsData = eventsData;
+    }
+
+    /**
+     * Get event to call depending on second marked by JSON
+     * Run event selected
+     * @param {number} second marked representation
+     */
+    getAndExecutedCurrentEvent(time) {
+      let _this = this;
+      let eventsMethodSelected = this.eventsData.filter(event => event.second === time);
+       if(eventsMethodSelected.length !== 0) {
+          eventsMethodSelected.forEach(function(val) {
+            let eventSelected = _this.events.find(event => event.method === val.type);
+            eventSelected.action(time, eventsMethodSelected);
+          })
+      }
+    }
+
+    /**
+     * Execute selected event each second
+     */
     run() {
-        // implement your code here...
-        console.log("RUNNING");
+      let time = 0;
+      let _this = this;
+      setInterval(function(){
+        _this.getCurrentEvent(time);
+        time++;
+      }, 1000);
     }
 };
