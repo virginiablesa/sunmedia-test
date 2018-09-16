d62d8384e1220e4663279bee6383842ff313220c
virginiablesa <43244317+virginiablesa@users.noreply.github.com>
2018-09-16 19:18:20 +0200

    Update EventManagerFactory.js

--- 4/src/EventManagerFactory.js
+++ 4/src/EventManagerFactory.js
@@ -1,9 +1,23 @@
 import EventManager from './EventManager';
 import Event from './Event';
 
+/** Class representing an event manager factory. */
 export default class EventManagerFactory{
+
+    /**
+     * Prepare events and action to execute depending on the received events and its types
+     * @param {Object} events - JSON Events
+     * @param {Array} Types - Events Type.
+     * @return {Function}
+     */
     static create(events, types) {
-        // implement your code here...
-        return new EventManager();
+      debugger;
+      let selectedEventsData = (events).filter(event => types.indexOf(event.type) !== -1);
+      let selectedEvents = [];
+      let func = (time, data) => console.log(`At second ${time}: ${JSON.stringify(data)}`);
+      selectedEventsData.forEach(function (event) {
+        selectedEvents.push(new Event(event, func))
+      });
+      return new EventManager(selectedEvents, selectedEventsData);
     }
 };
